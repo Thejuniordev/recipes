e3c672b7f58dcef428bcf59a06b486871dc3160f
Junior Alves <junioralves.webdeveloper@gmail.com>
2021-10-07 22:03:27 -0300

    Add images

--- lib/recipe.dart
+++ lib/recipe.dart
@@ -9,9 +9,9 @@ class Recipe {
   static List<Recipe> samples = [
     Recipe('Love Burguer', 'assets/food_1.jpeg'),
     Recipe('Combo burguer', 'assets/food_2.jpeg'),
-    Recipe('Balde de batata', 'assets/food_2.jpeg'),
-    Recipe('Burguer Tower', 'assets/food_2.jpeg'),
-    Recipe('Hot Dog', 'assets/food_2.jpeg'),
-    Recipe('Refrigerante', 'assets/food_2.jpeg'),
+    Recipe('Balde de batata', 'assets/food_3.jpeg'),
+    Recipe('Burguer Tower', 'assets/food_4.jpeg'),
+    Recipe('Hot Dog', 'assets/food_5.jpeg'),
+    Recipe('Refrigerante', 'assets/food_6.jpeg'),
   ];
 }
