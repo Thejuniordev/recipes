fb3a4061bbdd00ab6cd28cf32e568b91258bfeee
Junior Alves <junioralves.webdeveloper@gmail.com>
2021-10-07 21:29:36 -0300

    Displaying the list

--- lib/recipe.dart
+++ lib/recipe.dart
@@ -8,6 +8,10 @@ class Recipe {
   );
   static List<Recipe> samples = [
     Recipe('Love Burguer', 'assets/food_1.jpeg'),
-    Recipe('Love Burguer', 'assets/food_1.jpeg'),
+    Recipe('Combo burguer', 'assets/food_2.jpeg'),
+    Recipe('Balde de batata', 'assets/food_2.jpeg'),
+    Recipe('Burguer Tower', 'assets/food_2.jpeg'),
+    Recipe('Hot Dog', 'assets/food_2.jpeg'),
+    Recipe('Refrigerante', 'assets/food_2.jpeg'),
   ];
 }
